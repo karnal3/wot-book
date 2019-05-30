aca726403f3a1ef8a5b58163bdb58bcbde9f5e45
karnal3 <diwad1998@gmail.com>
2019-05-30 22:50:32 +0200

    sdfsdf

--- rozdzial04-gpio/dht.js
+++ rozdzial04-gpio/dht.js
@@ -1,6 +1,6 @@
 var sensorLib = require('node-dht-sensor');
 
-sensorLib.initialize(22, 12); //#A
+sensorLib.initialize(11, 12); //#A
 var interval = setInterval(function () { //#B
   read();
 }, 2000);
