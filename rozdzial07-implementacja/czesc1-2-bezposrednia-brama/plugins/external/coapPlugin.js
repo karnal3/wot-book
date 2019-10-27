fd15d8df21ca4f5343d1ca8268e554dcf5520687
karnal3 <diwad1998@gmail.com>
2019-10-27 11:27:54 +0100

    no message

--- rozdzial07-implementacja/czesc1-2-bezposrednia-brama/plugins/external/coapPlugin.js
+++ rozdzial07-implementacja/czesc1-2-bezposrednia-brama/plugins/external/coapPlugin.js
@@ -12,7 +12,7 @@ function connectHardware() {
     read: function () { //#B
       coap
         .request({ //#C
-          host: 'localhost',
+          host: '192.168.8.103',
           port: 5683,
           pathname: '/co2',
           options: {'Accept': 'application/json'}
